3a214f9b5ab223342ab0f95bee0289e457c549c1
Yuri <yuri.barru@gmail.com>
2023-08-17 21:00:23 +0700

    Deleted some unused pages

--- src/pages/customer-account-page.js
+++
@@ -1,8 +0,0 @@
-const { expect } = require('@playwright/test');
-
-exports.CustomerAccountPage = class CustomerAccountPage {
-    constructor(page) {
-        this.page = page;
-        this.greetings = "x"
-    }
-};
